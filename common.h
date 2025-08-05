fb4fae3c22625c3da5cef385abe9e7bfc91dd38e
witness <387902891@qq.com>
2025-08-05 15:46:57 +0800

    update gitigore

--- common.h
+++ common.h
@@ -1,7 +1,10 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <asm-generic/fcntl.h>
 #include <iostream>
+
+#define EPOLL_SIZE 5000
 
 struct Msg
 {
@@ -10,5 +13,16 @@ struct Msg
     int plateform;
     std::string content;
 };
+
+inline void addfd(int fd, int epfd, bool enable_et)
+{
+    struct epoll_event ev;
+    ev.data.fd = fd;
+    ev.events = EPOLLIN;
+    if(enable_et)
+        ev.events |= EPOLLET;
+    epoll_ctl( epfd, EPOLL_CTL_ADD, fd, &ev );
+    fcntl( fd, F_SETFL, fcntl( fd, F_GETFL ) | O_NONBLOCK );
+}
 
 #endif // COMMON_H
